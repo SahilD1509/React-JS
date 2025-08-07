3c53165bcee1bb969bd96e7bb8987c24a9c3d5ac
SahilD1509 <sdhiman1509@gmail.com>
2025-08-07 09:26:27 +0530

    React

---
+++ Class_1_React/react31/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client"
+let root = ReactDOM.createRoot(document.getElementById("root"))
+//root.render("Good Morning")
+//root.render(<h1>Good Morning</h1>)
+root.render(<div><h1>Good Morning</h1>
+                 <h2>Good Evening</h2></div>)
