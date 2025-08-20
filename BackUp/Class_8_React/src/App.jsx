92cfdd53169b6c22d7f54f6c1e25bd1c65c1c586
SahilD1509 <sdhiman1509@gmail.com>
2025-08-20 11:27:06 +0530

    'react-components'

--- BackUp/Class_8_React/src/App.jsx
+++ BackUp/Class_8_React/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './Navbar/navbar';
+import Employees from './Employees/employees';
 import Users from './Users/users';
 
 const App=()=>{
   return <div>
     <hr/>
     <Navbar/>
+    <Employees/>
     <Users/>
   </div>
 }
---
+++ BackUp/Class_8_React/src/Users/users.jsx
@@ -0,0 +1,13 @@
+import Axios from 'axios'
+let Users=()=>{
+    let getUserData = ()=>{
+        Axios.get('https://jsonplaceholder.typicode.com/users')
+        .then((resp)=>{console.log(resp.data)})
+        .catch(()=>{})
+    }
+    return <div>
+                <h3>User Component</h3>
+                <button onClick={getUserData}>Get Users</button>
+            </div>
+}
+export default Users;
